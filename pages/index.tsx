870268ef7e00711653ac4ccce64296e59c65295d
Henry Stoupe <h.stoupe@gmail.com>
2020-11-03 00:13:16 +1300

    Very basic form outline

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,66 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import styles from "../styles/index.module.scss";
 
 const HomePage = () => {
+  const [currentUser, setCurrentUser] = useState("Henry");
 
-  const [count, setCount] = useState(0);
+  const [numSerious, setNumSerious] = useState(0);
+  const [numBelayers, setNumBelayers] = useState(0);
+  const [numClimbers, setNumClimbers] = useState(0);
 
-  return <div>
-    <div>{count}</div>
-    <button onClick={() => setCount((c) => c+1)}>+</button>
+  const [notes, setNotes] = useState("");
+
+  const makeBooking = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submitted form");
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.formContainer}>
+        <h1>MAKE BOOKING</h1>
+        <form
+          action="makeBooking"
+          onSubmit={(e) => makeBooking(e)}
+          className={styles.form}
+        >
+          <h4>Serious</h4>
+          <input
+            type="number"
+            name="numSerious"
+            id="numSeriousInput"
+            value={numSerious}
+            onChange={(e) => {
+              setNumSerious(e.target.valueAsNumber);
+            }}
+          />
+          <h4>Belayers</h4>
+          <input
+            type="number"
+            name="numBelayers"
+            id="numBelayersInput"
+            value={numBelayers}
+            onChange={(e) => {
+              setNumBelayers(e.target.valueAsNumber);
+            }}
+          />
+          <h4>Climbers</h4>
+          <input
+            type="number"
+            name="numClimbers"
+            id="numClimbersInput"
+            value={numClimbers}
+            onChange={(e) => {
+              setNumClimbers(e.target.valueAsNumber);
+            }}
+          />
+          <h4>Time</h4>
+          <input type="datetime-local" name="date" id="date" />
+          <button type="submit">Sumbit</button>
+        </form>
+      </div>
     </div>
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
