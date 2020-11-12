d43ce119e27742a0775b61d97c2064701cec2847
Stoupe <h.stoupe@gmail.com>
2020-11-13 01:01:00 +1300

    Better styling for index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,43 +4,48 @@ import { Button } from "@material-ui/core";
 import { logIn, logOut } from "../functions/authFunctions";
 import { UserContext } from "../components/Contexts";
 import firebase from "firebase/app";
+import styles from "../styles/index.module.scss";
 
 const HomePage = () => {
   const { user, setUser } = useContext(UserContext);
 
   return (
-    <>
-      <Link href="dashboard">
-        <a>Dashboard</a>
-      </Link>
-
-      {!user ? (
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={async () => {
-            const user = await logIn();
-            setUser(user);
-          }}
-        >
-          Log In
-        </Button>
-      ) : (
-        <>
-          <p>Hello, {user.displayName}</p>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        {!user ? (
           <Button
             variant="contained"
-            color="secondary"
+            color="primary"
             onClick={async () => {
-              const user = await logOut();
+              const user = await logIn();
               setUser(user);
             }}
           >
-            Log Out
+            Log In
           </Button>
-        </>
-      )}
-    </>
+        ) : (
+          <>
+            <p>Hello, {user.displayName}</p>
+            <Link href="dashboard">
+              <Button variant="contained" color="primary">
+                Dashboard
+              </Button>
+              {/* <a>Dashboard</a> */}
+            </Link>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={async () => {
+                const user = await logOut();
+                setUser(user);
+              }}
+            >
+              Log Out
+            </Button>
+          </>
+        )}
+      </div>
+    </div>
   );
 };
 
