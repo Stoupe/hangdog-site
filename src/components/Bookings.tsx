db5ca2858833eb7acca9f93fcc2904b4a4728eb1
Stoupe <h.stoupe@gmail.com>
2020-11-23 12:59:27 +1300

    work on bookings logic

--- src/components/Bookings.tsx
+++ src/components/Bookings.tsx
@@ -8,38 +8,86 @@ import { add, addDays, format, getDay } from "date-fns";
 import { formatDay, formatHour } from "../functions/formatTime";
 import { bookingHours } from "./variables";
 import { Button } from "@material-ui/core";
+import { time } from "console";
 
 const Bookings: React.FC = () => {
-  // const test: BookingType = {
-  //   id: "",
-  //   createdAt: null,
-  //   bookingDate: "",
-  //   bookingName: "string",
-  //   bookingNotes: "",
-  //   bookingTime: "", //TODO time?
-  //   createdBy: "",
-  //   numSerious: 1,
-  //   numBelayers: 1,
-  //   numClimbing: 1,
-  //   numRopes: 1,
-  //   totalNumInGym: 1,
-  // };
+  const sampleBooking = {
+    bookingId: "LhQlmzK3UsbrcRHbJMyI",
+    bookingTime: "5pm",
+    numSerious: 0,
+    createdAt: { seconds: 1606045996, nanoseconds: 874000000 },
+    bookingNotes: "",
+    bookingName: "Test",
+    totalNumInGym: 4,
+    numBelayers: 1,
+    numClimbers: 3,
+    bookingDate: "23/11/2020",
+    numRopes: 1,
+    createdBy: "Henry Stoupe",
+  };
 
-  let timeData = {};
+  const a = {
+    "5pm": {
+      bookingId: "aS2nfjUWzRUxHi8Mr0Rj",
+      bookingTime: "5pm",
+      numBelayers: 4,
+      numRopes: 5,
+      createdAt: { seconds: 1606084615, nanoseconds: 736000000 },
+      bookingNotes: "",
+      createdBy: "Henry Stoupe",
+      bookingName: "Sal",
+      numClimbers: 2,
+      numSerious: 2,
+      bookingDate: "23/11/2020",
+      totalNumInGym: 8,
+    },
+  };
+
+  type DayBookings = {
+    "0000"?: BookingType[];
+    "0100"?: BookingType[];
+    "0200"?: BookingType[];
+    "0300"?: BookingType[];
+    "0400"?: BookingType[];
+    "0500"?: BookingType[];
+    "0600"?: BookingType[];
+    "0700"?: BookingType[];
+    "0800"?: BookingType[];
+    "0900"?: BookingType[];
+    "1000"?: BookingType[];
+    "1100"?: BookingType[];
+    "1200"?: BookingType[];
+    "1300"?: BookingType[];
+    "1400"?: BookingType[];
+    "1500"?: BookingType[];
+    "1600"?: BookingType[];
+    "1700"?: BookingType[];
+    "1800"?: BookingType[];
+    "1900"?: BookingType[];
+    "2000"?: BookingType[];
+    "2100"?: BookingType[];
+    "2200"?: BookingType[];
+    "2300"?: BookingType[];
+    "2400"?: BookingType[];
+  };
+
+  // let timeData = {};
 
   // const [data, setData] = useState<[BookingType[]] | []>([]);
   const [data, setData] = useState([]);
+  const [bookings, setBookings] = useState({});
 
   const updateBookingDisplay = () => {
+    data.forEach((booking) => {
+      const time: string = booking.bookingTime;
 
-    data.forEach(booking => {
-      const time = booking.bookingTime;
+      const allBookingsForTime = bookings[time] || [];
 
-      timeData[time] = {
-        
-      }
-    })
-
+      setBookings((prevState) => ({
+        ...prevState,
+        [time]: [allBookingsForTime],
+      }));
+    });
   };
 
   const fetchData = async () => {
@@ -57,13 +105,17 @@ const Bookings: React.FC = () => {
       const id: string = doc.id;
       const bookingData = doc.data();
 
-      bookingArray.push({ id: id, ...bookingData });
+      bookingArray.push({ bookingId: id, ...bookingData });
     });
 
     setData(bookingArray);
 
     updateBookingDisplay();
   };
+
+  useEffect(() => {
+    updateBookingDisplay();
+  }, [data]);
 
   useEffect(() => {
     fetchData();
@@ -73,8 +125,6 @@ const Bookings: React.FC = () => {
     //   clearInterval(interval);
     // };
   }, []);
-
-  console.log(data);
 
   const renderHour = (hour: number) => {
     if (data) {
@@ -103,6 +153,7 @@ const Bookings: React.FC = () => {
       <h1 className={styles.containerTitle}>Today</h1>
       <div className={styles.innerContainer}>
         <p>{JSON.stringify(data)}</p>
+        <p>{JSON.stringify(bookings)}</p>
         <Button onClick={fetchData}>FETCH DATA</Button>
         {bookingHours[formatDay(getDay(addDays(new Date(), 0)))].map(
           (e: number) => {
