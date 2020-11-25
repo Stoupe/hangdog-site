42780b8a7456dc49f862e809d2182d840a713f1b
Stoupe <h.stoupe@gmail.com>
2020-11-25 18:16:01 +1300

    Add mock code for notes section

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 import styles from "../styles/Notes.module.scss";
 
@@ -6,8 +7,21 @@ const Notes: React.FC = () => {
     <div className={styles.outerContainer}>
       <div className={styles.containerHeader}>
         <h1 className={styles.containerTitle}>Notes</h1>
+        <Button variant="contained">+</Button>
       </div>
-      <div className={styles.innerContainer}></div>
+      <div className={styles.innerContainer}>
+        <div className={styles.noteContainer}>
+          <div className={styles.noteMessage}>
+            Code for the shop padlock is 1234
+          </div>
+          <div className={styles.noteFooter}>
+            <div className={styles.noteAuthor}>Henry - 11/11/20</div>
+            <div className={styles.dismissNote}>
+              <Button variant="contained">Done</Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
